672f12584bdf9419e82cfe98c1a9c7f3752cbe06
Jan Vlcek <vlki@vlki.cz>
2018-06-21 22:14:31 +1200

    Link tooltip in rich text editor

--- app/javascript/admin/components/RichTextEditor/featurePlugins/Link.tsx
+++ app/javascript/admin/components/RichTextEditor/featurePlugins/Link.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { Popover, Position } from '@blueprintjs/core';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as Slate from 'slate';
@@ -46,6 +47,13 @@ const unwrapLink = (change: Slate.Change) => {
   return change.unwrapInline('link');
 };
 
+const setLinkHref = (change: Slate.Change, href: string) => {
+  return change.setInlines({
+    type: 'link',
+    data: { href },
+  });
+};
+
 const hasLinks = (value: Slate.Value) =>
   value.inlines.some((inline) => (inline ? inline.type === 'link' : false));
 
@@ -89,13 +97,60 @@ const renderToolbarButton = (props: IRenderToolbarButtonProps) => {
 };
 
 const LinkNode = (props: RenderNodeProps) => {
-  const { children, attributes, node } = props;
+  const { children, attributes, node, isSelected, editor } = props;
 
   const href = node.data.get('href');
 
+  const onLinkMouseDown = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    window.open(href, '_blank');
+  };
+
+  const onEditMouseDown = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    const newHref = window.prompt('Vložte URL odkazu (např. https://demagog.cz/):', href);
+    if (newHref === null || newHref === '') {
+      return;
+    }
+
+    if (editor.props.onChange) {
+      editor.props.onChange((editor.value.change() as any).call(setLinkHref, newHref));
+    }
+  };
+
+  const onRemoveMouseDown = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    if (editor.props.onChange) {
+      editor.props.onChange(editor.value.change().call(unwrapLink));
+    }
+  };
+
   return (
-    <a {...attributes} href={href} style={{ textDecoration: 'underline', cursor: 'text' }}>
-      {children}
-    </a>
+    <Popover
+      autoFocus={false}
+      content={
+        <div style={{ zIndex: 10000, padding: '5px 10px', maxWidth: 400 }} className="small">
+          <a href={href} onMouseDown={onLinkMouseDown}>
+            {href}
+          </a>{' '}
+          –{' '}
+          <a href="#" onMouseDown={onEditMouseDown}>
+            Změnit
+          </a>{' '}
+          |{' '}
+          <a href="#" onMouseDown={onRemoveMouseDown}>
+            Odstranit
+          </a>
+        </div>
+      }
+      isOpen={isSelected}
+      position={Position.BOTTOM_LEFT}
+    >
+      <a {...attributes} href={href} style={{ textDecoration: 'underline', cursor: 'text' }}>
+        {children}
+      </a>
+    </Popover>
   );
 };
