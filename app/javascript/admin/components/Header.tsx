ac9ec28edf4db2293bdf2ed81c6897068f2f85c9
Jan Vlcek <vlki@vlki.cz>
2018-07-22 16:38:51 +0200

    Allow admins to become another user

--- app/javascript/admin/components/Header.tsx
+++ app/javascript/admin/components/Header.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 
-import { Alignment, AnchorButton, Button, Classes, Colors, Navbar } from '@blueprintjs/core';
+import {
+  Alignment,
+  AnchorButton,
+  Button,
+  Classes,
+  Colors,
+  Dialog,
+  Intent,
+  Navbar,
+} from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import * as classNames from 'classnames';
 import { Query } from 'react-apollo';
@@ -9,7 +18,9 @@ import { NavLink } from 'react-router-dom';
 
 import { GetNotificationsQuery, GetNotificationsQueryVariables } from '../operation-result-types';
 import { GetNotifications } from '../queries/queries';
-import { IState } from '../reducers';
+import { IState as ReduxState } from '../reducers';
+import Authorize from './Authorize';
+import UserSelect from './forms/controls/UserSelect';
 
 class GetNotificationsQueryComponent extends Query<
   GetNotificationsQuery,
@@ -17,70 +28,147 @@ class GetNotificationsQueryComponent extends Query<
 > {}
 
 interface IProps {
-  currentUser: IState['currentUser']['user'];
+  currentUser: ReduxState['currentUser']['user'];
 }
 
-function Header(props: IProps) {
-  return (
-    <Navbar fixedToTop>
-      <Navbar.Group align={Alignment.LEFT}>
-        <Navbar.Heading>
-          <NavLink to="/admin" style={{ color: Colors.DARK_GRAY1 }}>
-            Demagog
-          </NavLink>
-        </Navbar.Heading>
-      </Navbar.Group>
+interface IState {
+  showBecomeAnotherUserDialog: boolean;
+}
 
-      <Navbar.Group align={Alignment.RIGHT}>
-        <GetNotificationsQueryComponent query={GetNotifications} variables={{ includeRead: false }}>
-          {({ data, loading, error }) => {
-            if (loading || !data) {
-              return null;
-            }
+class Header extends React.Component<IProps, IState> {
+  public state = {
+    showBecomeAnotherUserDialog: false,
+  };
 
-            if (error) {
-              console.error(error); // tslint:disable-line:no-console
-              return null;
-            }
+  public toggleBecomeAnotherUserDialog = () => {
+    this.setState({ showBecomeAnotherUserDialog: !this.state.showBecomeAnotherUserDialog });
+  };
 
-            return (
+  public render() {
+    return (
+      <>
+        {this.state.showBecomeAnotherUserDialog && (
+          <BecomeAnotherUserDialog onCancel={this.toggleBecomeAnotherUserDialog} />
+        )}
+
+        <Navbar fixedToTop>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>
+              <NavLink to="/admin" style={{ color: Colors.DARK_GRAY1 }}>
+                Demagog
+              </NavLink>
+            </Navbar.Heading>
+          </Navbar.Group>
+
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Authorize permissions={['admin:become-another-user']}>
               <>
-                <NavLink
-                  to="/admin/notifications"
-                  className={classNames(
-                    Classes.BUTTON,
-                    Classes.iconClass(IconNames.NOTIFICATIONS),
-                    {
-                      [Classes.MINIMAL]: data.notifications.total_count === 0,
-                      [Classes.INTENT_PRIMARY]: data.notifications.total_count > 0,
-                    },
-                  )}
-                >
-                  {`${data.notifications.total_count}`}
-                </NavLink>
+                <Button
+                  icon={IconNames.PEOPLE}
+                  minimal
+                  onClick={this.toggleBecomeAnotherUserDialog}
+                />
                 <Navbar.Divider />
               </>
-            );
-          }}
-        </GetNotificationsQueryComponent>
+            </Authorize>
 
-        {props.currentUser !== null && (
-          <>
-            <Button
-              icon={IconNames.USER}
-              minimal
-              text={`${props.currentUser.first_name} ${props.currentUser.last_name}`}
-            />
-            <Navbar.Divider />
-          </>
-        )}
-        <AnchorButton icon={IconNames.LOG_OUT} minimal text="Odhlásit se" href="/sign_out" />
-      </Navbar.Group>
-    </Navbar>
-  );
+            <GetNotificationsQueryComponent
+              query={GetNotifications}
+              variables={{ includeRead: false }}
+            >
+              {({ data, loading, error }) => {
+                if (loading || !data) {
+                  return null;
+                }
+
+                if (error) {
+                  console.error(error); // tslint:disable-line:no-console
+                  return null;
+                }
+
+                return (
+                  <>
+                    <NavLink
+                      to="/admin/notifications"
+                      className={classNames(
+                        Classes.BUTTON,
+                        Classes.iconClass(IconNames.NOTIFICATIONS),
+                        {
+                          [Classes.MINIMAL]: data.notifications.total_count === 0,
+                          [Classes.INTENT_PRIMARY]: data.notifications.total_count > 0,
+                        },
+                      )}
+                    >
+                      {`${data.notifications.total_count}`}
+                    </NavLink>
+                    <Navbar.Divider />
+                  </>
+                );
+              }}
+            </GetNotificationsQueryComponent>
+
+            {this.props.currentUser !== null && (
+              <>
+                <Button
+                  icon={IconNames.USER}
+                  minimal
+                  text={`${this.props.currentUser.first_name} ${this.props.currentUser.last_name}`}
+                />
+                <Navbar.Divider />
+              </>
+            )}
+            <AnchorButton icon={IconNames.LOG_OUT} minimal text="Odhlásit se" href="/sign_out" />
+          </Navbar.Group>
+        </Navbar>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = (state: IState) => ({
+interface IBecomeAnotherUserDialogProps {
+  onCancel: () => void;
+}
+
+interface IBecomeAnotherUserDialogState {
+  userId: string | null;
+}
+
+class BecomeAnotherUserDialog extends React.Component<
+  IBecomeAnotherUserDialogProps,
+  IBecomeAnotherUserDialogState
+> {
+  public state = {
+    userId: null,
+  };
+
+  public render() {
+    const { onCancel } = this.props;
+
+    const { userId } = this.state;
+
+    return (
+      <Dialog isOpen onClose={onCancel} title="Přihlásit se jako někdo jiný">
+        <div className={Classes.DIALOG_BODY}>
+          <UserSelect value={userId} onChange={(value) => this.setState({ userId: value })} />
+        </div>
+        <div className={Classes.DIALOG_FOOTER}>
+          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+            <Button text="Zpět" onClick={onCancel} />
+            <AnchorButton
+              href={`/admin/become-another-user/${userId}`}
+              disabled={userId === null}
+              text="Přihlásit"
+              onClick={onCancel}
+              intent={Intent.PRIMARY}
+            />
+          </div>
+        </div>
+      </Dialog>
+    );
+  }
+}
+
+const mapStateToProps = (state: ReduxState) => ({
   currentUser: state.currentUser.user,
 });
 
