0273bf204d3734f6959ddb9caab2bab877b61943
Vaclav Bohac <bohac.v@gmail.com>
2019-03-31 19:40:36 +0200

    fixup! Refactor graphql types to classes

--- test/graphql/types/query_type/content_images_test.rb
+++ test/graphql/types/query_type/content_images_test.rb
@@ -35,23 +35,4 @@ class QueryTypeContentImagesTest < GraphQLTestCase
     assert_equal 2, result.data.contentImages.totalCount
     assert_equal [second.id.to_s, first.id.to_s], result.data.contentImages.items.map(&:id)
   end
-
-=begin
-  test "notifications should return also read ones if include_read flag is sent" do
-    user = create(:user)
-    create_list(:notification, 2, recipient: user)
-    create(:notification, recipient: user, read_at: Time.now)
-
-    query_string = "
-       query {
-         notifications(includeRead: true) {
-           totalCount
-         }
-       }"
-
-    result = execute(query_string, context: authenticated_user_context(user: user))
-
-    assert_equal 3, result.data.notifications.totalCount
-  end
-=end
 end
