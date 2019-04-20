c4db297d0e4e2883cfb98df536ca0b2e47a2f6a2
Jan Vlcek <vlki@vlki.cz>
2019-04-20 21:18:36 +0200

    Rubocop offense

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,7 +4,7 @@ class PageController < ApplicationController
   def show
     @page = Page.published.friendly.find(params[:slug])
 
-    if @page.template == 'about_us'
+    if @page.template == "about_us"
       @users = User.where(active: true, user_public: true).order(rank: :asc, last_name: :asc) if params[:slug] == "o-nas"
     end
   end
