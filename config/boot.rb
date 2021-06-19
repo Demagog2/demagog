c117077db721621f72e5867c3bae3e53ecd8f0b0
Vaclav Bohac <bohac.v@gmail.com>
2021-06-19 13:52:15 +0200

    Remove warning deprecation silencing

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-# Ignore ruby-2.7.* deprecation messages
-Warning[:deprecated] = false
