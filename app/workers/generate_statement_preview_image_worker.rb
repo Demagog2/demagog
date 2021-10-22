4f35c387ab91cef1ffba35e2e7effbd952715572
Jan Vlcek <vlki@vlki.cz>
2021-10-22 11:00:42 +0200

    Generate statement preview image worker: do not throw exception when statement is not published

--- app/workers/generate_statement_preview_image_worker.rb
+++ app/workers/generate_statement_preview_image_worker.rb
@@ -5,6 +5,15 @@ class GenerateStatementPreviewImageWorker
   sidekiq_options retry: 5
 
   def perform(statement_id, debug = false)
+    statement = Statement.find_by(id: statement_id)
+
+    if !statement || !statement.published
+      # The statement might have been deleted or unpublished in the meantime,
+      # in that case we dont do anything, because if it gets published again,
+      # new worker will get spawned
+      return
+    end
+
     GenerateStatementPreviewImageService.generate(statement_id, debug)
   end
 end
