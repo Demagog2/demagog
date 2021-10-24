2290eff9e63f79bff45c4d32d04f44b0e87a76c8
Jan Vlcek <vlki@vlki.cz>
2021-10-24 15:26:51 +0200

    Speakers v2 (#661)

---
+++ app/javascript/application/controllers/speaker/index_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from 'stimulus';
+import * as queryString from 'query-string';
+
+export default class extends Controller {
+  static targets = ['filtersForm'];
+
+  onFilterSelectChange() {
+    this.buildAndGoToFiltersUrl();
+  }
+
+  onFulltextConfirmButton() {
+    this.buildAndGoToFiltersUrl();
+  }
+
+  onFiltersListSubmit(e) {
+    this.buildAndGoToFiltersUrl();
+
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  buildAndGoToFiltersUrl() {
+    const noFiltersUrl = this.data.get('noFiltersUrl');
+    const queryParams = {};
+
+    this.filtersFormTarget.querySelectorAll('input, select').forEach((el) => {
+      if (el.type === 'checkbox') {
+        if (el.checked) {
+          queryParams[el.name] = el.value;
+        }
+      } else {
+        if (el.value != '') {
+          queryParams[el.name] = el.value;
+        }
+      }
+    });
+
+    let queryParamsAsString = queryString.stringify(queryParams, { arrayFormat: 'bracket' });
+    if (queryParamsAsString !== '') {
+      queryParamsAsString = '?' + queryParamsAsString;
+    }
+
+    window.location = noFiltersUrl + queryParamsAsString;
+  }
+}
