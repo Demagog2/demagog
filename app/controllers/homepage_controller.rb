8ae195ca792b70975a0353003800721ff91c65a5
Jan Vlcek <vlki@vlki.cz>
2022-03-04 13:07:49 +0100

    New homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomepageController < FrontendController
-  def index
+  def index_old
     cover_story = Article.kept.published.for_homepage.order(published_at: :desc).first
 
     unless params[:page].present?
@@ -12,14 +12,20 @@ class HomepageController < FrontendController
     end
 
     @articles =
-      Article.kept.published.for_homepage.order(published_at: :desc).where.not(id: cover_story).page(
-        params[:page]
-      )
+      Article.kept.published.for_homepage.order(published_at: :desc).where.not(id: cover_story)
+        .page(params[:page])
         .per(10)
 
     # return unless Rails.env.production?
 
     # TODO: revisit cache headers and do properly
     # expires_in 1.hour, public: true
+  end
+
+  def index
+    @page_number = params[:page]
+
+    @articles =
+      Article.kept.published.for_homepage.order(published_at: :desc).page(@page_number).per(10)
   end
 end
