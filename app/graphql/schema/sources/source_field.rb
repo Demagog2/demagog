51a0c2bbc952363b4c78b2c15fb02516c38db47b
vaclavbohac <vaclav.bohac@productboard.com>
2022-11-11 12:27:53 +0100

    Extract sources from main query type

---
+++ app/graphql/schema/sources/source_field.rb
@@ -0,0 +1,15 @@
+module Schema::Sources::SourceField
+  extend ActiveSupport::Concern
+
+  included do
+    field :source, Types::SourceType, null: false do
+      argument :id, GraphQL::Types::Int, required: true
+    end
+
+    def source(id:)
+      Source.find(id)
+    rescue ActiveRecord::RecordNotFound
+      raise GraphQL::ExecutionError.new("Could not find Source with id=#{id}")
+    end
+  end
+end
