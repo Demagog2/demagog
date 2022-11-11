6ddb2d5d81adc0ae03158dc5e8677b61f72239df
vaclavbohac <vaclav.bohac@productboard.com>
2022-11-11 10:30:47 +0100

    Add component tests

--- test/components/image_component_test.rb
+++ test/components/image_component_test.rb
@@ -3,10 +3,25 @@
 require "test_helper"
 
 class ImageComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ImageComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_component_renders_lazy_loaded_image
+    render_inline(ImageComponent.new(src: src, alt: alt, lazy: true))
+
+    assert_selector("img[src=\"#{src}\"][alt=\"#{alt}\"][loading=lazy]", count: 1)
   end
+
+  def test_component_renders_non_lazy_loaded_image
+    render_inline(ImageComponent.new(src: src, alt: alt, lazy: false))
+
+    assert_selector("img[src=\"#{src}\"][alt=\"#{alt}\"][loading=lazy]", count: 0)
+    assert_selector("img[src=\"#{src}\"][alt=\"#{alt}\"]", count: 1)
+  end
+
+  private
+    def src
+      "http://example.com"
+    end
+
+    def alt
+      "Alt text"
+    end
 end
