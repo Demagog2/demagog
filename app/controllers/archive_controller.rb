4bc8aca321ea53485d94618f384563e8f42e9447
vaclavbohac <vaclav.bohac@productboard.com>
2022-11-16 16:09:26 +0100

    Fix pagination on the archive page

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -6,7 +6,5 @@ class ArchiveController < ApplicationController
       .where(published: true)
       .order(published_at: :desc)
       .page(params[:page])
-
-    # expires_in 1.hour, public: true
   end
 end
