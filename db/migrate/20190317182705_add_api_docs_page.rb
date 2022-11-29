25c0183f9c8f5740dcb932624b82f781cfb5d3d7
vaclavbohac <vaclav.bohac@productboard.com>
2022-11-29 20:37:16 +0100

    fixup! Mute problematic migrations

--- db/migrate/20190317182705_add_api_docs_page.rb
+++ db/migrate/20190317182705_add_api_docs_page.rb
@@ -4,7 +4,7 @@ class AddApiDocsPage < ActiveRecord::Migration[5.2]
   def change
     page = Page.create(title: "API pro vývojáře", slug: "api-pro-vyvojare", published: true, template: "api_docs")
 
-    menu_item_order_last = MenuItem.all.map { |menu_item| menu_item.order }.max
+    menu_item_order_last = MenuItem.all.map { |menu_item| menu_item.order }.max || 0
     MenuItem.create(title: "API pro vývojáře", kind: "page", page: page, order: menu_item_order_last + 1)
   end
 end
