1256bbe6a4d40a792dcf1ae373cafbd877df27b2
Stodulka <stodulka@musenaut.com>
2022-12-28 14:20:53 +0100

    About pages

--- app/javascript/application/controllers/components/sticky_controller.js
+++ app/javascript/application/controllers/components/sticky_controller.js
@@ -4,18 +4,27 @@ export default class extends Controller {
 
   static targets = [ 'sticky', 'content' ]
 
+  initialize() {
+    this.breakpoint = this.data.get('breakpoint');
+  }
+
   onScroll(event) {
     if (this.stickyTarget && this.contentTarget) {
-      const sH = this.stickyTarget.getBoundingClientRect().height;
-      const sW = this.stickyTarget.getBoundingClientRect().width;
-      const cH = this.contentTarget.getBoundingClientRect().height;
-      const cW = this.contentTarget.getBoundingClientRect().width;
 
-      if(cH > sH && sW < cW){
+
+      if(window.innerWidth > this.breakpoint){
+        const sH = this.stickyTarget.getBoundingClientRect().height;
+        const sW = this.stickyTarget.getBoundingClientRect().width;
+        const cH = this.contentTarget.getBoundingClientRect().height;
+        const cW = this.contentTarget.getBoundingClientRect().width;
+        
         this.sticked(sW, sH, cW, cH);
       } else {
         if (this.stickyTarget.getAttribute('style')) {
           this.stickyTarget.removeAttribute('style')
+        }
+        if (this.stickyTarget.classList.contains("position-absolute")) {
+          this.stickyTarget.classList.remove("position-absolute");
         }
       }
     }
@@ -23,20 +32,29 @@ export default class extends Controller {
   }
 
   sticked(sW, sH, cW, cH) {
-    const dis = 40;
+    const dis = 80;
     const offset = this.contentTarget.offsetTop;
     const sT = window.scrollY;
-    const eT = (this.element.getBoundingClientRect().top * -1) + offset;
+    const eT = (this.contentTarget.getBoundingClientRect().top * -1) + dis;
 
     if (eT > 0) {
       const max = cH - sH;
       if (eT <= max) {
-        this.stickyTarget.setAttribute("style", "top: " + eT +"px; position: absolute;")
+        this.stickyTarget.setAttribute("style", "top: " + eT +"px;")
+        if (! this.stickyTarget.classList.contains("position-absolute")) {
+          this.stickyTarget.classList.add("position-absolute");
+        }
       } else {
-        this.stickyTarget.setAttribute("style", "top: " + max +"px; position: absolute;")
+        if (! this.stickyTarget.classList.contains("position-absolute")) {
+          this.stickyTarget.classList.add("position-absolute");
+        }
+        this.stickyTarget.setAttribute("style", "top: " + max +"px;")
       }
 
     } else {
+      if (this.stickyTarget.classList.contains("position-absolute")) {
+        this.stickyTarget.classList.remove("position-absolute");
+      }
       if (this.stickyTarget.getAttribute('style')) {
         this.stickyTarget.removeAttribute('style')
       }
