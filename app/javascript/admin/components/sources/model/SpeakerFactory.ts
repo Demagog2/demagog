dc5345e66669a4926264de0dabe9407b99bf53cd
vaclavbohac <vaclav.bohac@productboard.com>
2023-02-12 10:23:02 +0100

    Remove unused code

--- app/javascript/admin/components/sources/model/SpeakerFactory.ts
+++
@@ -1,6 +0,0 @@
-import { Factory } from 'fishery';
-import { Speaker } from './Speaker';
-
-export const speakerFactory = Factory.define<Speaker>(({ sequence }) => {
-  return new Speaker(String(sequence), `First name ${sequence}`, 'Doe');
-});
