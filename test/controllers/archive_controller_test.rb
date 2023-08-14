6ac1c48d89d95486a37426acdb1f5ea6103f8d3b
Vaclav Bohac <bohac.v@gmail.com>
2023-08-14 20:05:33 +0200

    Remove rest of the usages of the article_type entity

--- test/controllers/archive_controller_test.rb
+++ test/controllers/archive_controller_test.rb
@@ -3,7 +3,15 @@
 require "test_helper"
 
 class ArchiveControllerTest < ActionDispatch::IntegrationTest
-  test "should render index page" do
+  test "should render empty index page" do
+    get archive_url
+    assert_response :success
+  end
+
+  test "should render index page with articles" do
+    create_list(:article, 2)
+    create_list(:static, 2)
+
     get archive_url
     assert_response :success
   end
