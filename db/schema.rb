18866901c6301fb4c16e47ca060ae4e31a4aa271
Vaclav Bohac <bohac.v@gmail.com>
2023-08-16 18:03:51 +0200

    Migrate existing assessments (#956)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_062021) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_143128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
