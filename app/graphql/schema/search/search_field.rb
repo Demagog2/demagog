e4d40620369b53035983a77dd10f6db647df96d7
Vaclav Bohac <bohac.v@gmail.com>
2023-12-02 12:38:32 +0100

    Add (published and factual) statements search to the GraphQL API (#996)

--- app/graphql/schema/search/search_field.rb
+++ app/graphql/schema/search/search_field.rb
@@ -6,5 +6,6 @@ module Schema::Search::SearchField
   included do
     field :search_speakers, Schema::Search::Types::SearchResultSpeakerType, null: false, resolver: Schema::Search::Resolvers::SpeakerSearchResultResolver
     field :search_articles, Schema::Search::Types::SearchResultArticleType, null: false, resolver: Schema::Search::Resolvers::ArticleSearchResultResolver
+    field :search_statements, Schema::Search::Types::SearchResultStatementType, null: false, resolver: Schema::Search::Resolvers::StatementSearchResultResolver
   end
 end
