983e3db0159eeba6e10713c9172b59207d5c84a5
Vaclav Bohac <bohac.v@gmail.com>
2023-12-18 17:32:05 +0000

    Add article tag by slug to the GraphQL API

--- app/graphql/types/article_tag_type.rb
+++ app/graphql/types/article_tag_type.rb
@@ -11,5 +11,14 @@ module Types
     field :published, Boolean, null: true
     field :stats, String, null: true
     field :order, String, null: false
+
+    field :articles, [Types::ArticleType], null: true do
+      argument :limit, Int, required: false, default_value: 10
+      argument :offset, Int, required: false, default_value: 0
+    end
+
+    def articles(limit:, offset:)
+      object.articles.published.order(published_at: :desc).limit(limit).offset(offset)
+    end
   end
 end
