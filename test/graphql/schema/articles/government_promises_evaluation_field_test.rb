2ffee03711545c84337066c58747cbdbd2807f27
vaclavbohac <vaclav.bohac@productboard.com>
2024-02-20 16:26:41 +0100

    Add perex to government promises evaluation articles

--- test/graphql/schema/articles/government_promises_evaluation_field_test.rb
+++ test/graphql/schema/articles/government_promises_evaluation_field_test.rb
@@ -30,6 +30,7 @@ module Schema::Articles
           id
           slug
           title
+          perex
           promiseCount
           promises {
             id
@@ -42,6 +43,7 @@ module Schema::Articles
 
       assert_equal article.slug, result.data.governmentPromisesEvaluationBySlug.slug
       assert_equal article.title, result.data.governmentPromisesEvaluationBySlug.title
+      assert_equal article.perex, result.data.governmentPromisesEvaluationBySlug.perex
       assert_equal 1, result.data.governmentPromisesEvaluationBySlug.promiseCount
       assert_equal statement.id.to_s, result.data.governmentPromisesEvaluationBySlug.promises.first.id
     end
